d9088cb2960cc0b185ac9070c0570d5cf2f0abad
cagridemirtash <cagridemirtash@gmail.com>
2022-12-21 14:53:29 +0300

    Front Structure
    Divide the all project redesign by microsoft typescript template.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,3 @@
-
-import './App.css';
-import Cagri from "../src/component/cagri"
-import { useState } from 'react';
 // Login -> Authentication
 // Navbar -> Bişiler Ekleriz
 // Main Nurse View -> Şu isimli hastanın şu şikayeti var -> Checkbox -> IsGood
@@ -10,23 +6,21 @@ import { useState } from 'react';
 // Which nurse is related to Patient. -> Database
 // Save -> New view -> When patient clicked -> Send to Nurse
 // Patient Name, Yatak Numarası, IsComploted -> Şuan ilgileniliyor.
-async function getData() : Promise<any>{
-  return fetch('https://localhost:44350/WeatherForecast')
-                // the JSON body is taken from the response
-                .then(res => res.json())
-                .then(res => {
-                        // The response has an `any` type, so we need to cast
-                        // it to the `User` type, and return it from the promise
-                        return res as any
-                })
-}
+
+import * as React from 'react';
+import Hello from './components/Hello/Hello';
+
 function App() {
-  // console.log(getData())
   return (
-    <>
-      <h1>Mustafa Bir Java Geliştiricisidir.</h1>
-      <Cagri />
-    </>
+    <div className="App">
+      <div className="App-header">
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <Hello name="TypeScript" />
+    </div>
   );
 }
 
