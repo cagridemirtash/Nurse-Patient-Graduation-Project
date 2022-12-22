ebde6be7cc0c170446230a40d206292f33a3f09a
cagridemirtash <cagridemirtash@gmail.com>
2022-12-22 15:23:45 +0300

    Material UI
    Ready-to-use sign-in component added

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,15 @@
-
 import { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import {AppProps} from './types/AppProps'
-
+import Button from '@mui/material/Button'
 
 function App({name , secondName = "Default Text"} : AppProps){
   const [user, setUser] = useState("userName");
   
   useEffect(() => {
     console.log("Ben bir kere çalışıyorum")
+    setUser("Cagri");
   }, [])
   return (
     <>
@@ -17,6 +17,7 @@ function App({name , secondName = "Default Text"} : AppProps){
       <h1>{name}</h1>
       {secondName && <p>{secondName}</p>}
       {user}
+      <Button variant="contained">Hello World</Button>
     </>
   );
 }
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+/* Add Font from MaterialUI */
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+/* Add Font from MaterialUI End*/
+import Login from "./components/Login/SignIn/index"
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App name = "Merhaba Dünya"/>
+    {/* <App name = "Merhaba Dünya"/> */}
+    <Login />
   </React.StrictMode>
 );
 
