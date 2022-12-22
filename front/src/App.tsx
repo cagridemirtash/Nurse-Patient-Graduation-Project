b072437db1166902b47f671d354e72828ba4d4c3
cagridemirtash <cagridemirtash@gmail.com>
2022-12-22 13:48:30 +0300

    Material UI
    Add material-ui dependencies and collect type in types folder

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,18 +1,22 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
+import {AppProps} from './types/AppProps'
 
-interface AppProps {
-  name : string;
-  secondName?: string;
-}
 
 function App({name , secondName = "Default Text"} : AppProps){
+  const [user, setUser] = useState("userName");
+  
+  useEffect(() => {
+    console.log("Ben bir kere çalışıyorum")
+  }, [])
   return (
     <>
       <Navbar />
       <h1>{name}</h1>
       {secondName && <p>{secondName}</p>}
+      {user}
     </>
   );
 }
