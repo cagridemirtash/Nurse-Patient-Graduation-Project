664b9780330ed69d84cf7d6d683a162990cbbbe8
Çağrı Demirtaş <cagridemirtash@gmail.com>
2022-12-25 18:41:37 +0300

    The Huge Update
    Add admin dasboard and firebase config for sign-in

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,19 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Navbar from './components/Navbar/Navbar'
-import {AppProps} from './types/AppProps'
-import Button from '@mui/material/Button'
+import { useEffect, useState } from "react";
+import "./App.css";
+import AdminPage from "./containers/Admin";
+import Navbar from "./components/Navbar/Navbar";
+import { AppProps } from "./types/AppProps";
+import Button from "@mui/material/Button";
 
-function App(){
+function App() {
   const [user, setUser] = useState("userName");
-  
+
   useEffect(() => {
-    console.log("Ben bir kere çalışıyorum")
     setUser("Cagri");
-  }, [])
+  }, []);
   return (
     <>
-      <Navbar />
-      {user}
+      <AdminPage />
     </>
   );
 }
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,47 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import './index.css';
+import "./index.css";
 /* Add Font from MaterialUI */
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 /* Add Font from MaterialUI End*/
-import Root from "./routes/root"
-import Login from "./components/Login/SignIn/index"
-import SignUp from "./components/Login/SignUp/index"
+import Root from "./routes/root";
+import MainPage from "./App";
+import Login from "./components/Login/SignIn/index";
+import SignUp from "./components/Login/SignUp/index";
 import ErrorPage from "./components/Error/ErrorPage";
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/main",
+    element: <MainPage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/signIn",
     element: <Login />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
   },
   {
     path: "/signUp",
     element: <SignUp />,
-    errorElement: <ErrorPage />
-  }
+    errorElement: <ErrorPage />,
+  },
 ]);
 
 root.render(
   <React.StrictMode>
-    {/* <App name = "Merhaba Dünya"/> */}
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
