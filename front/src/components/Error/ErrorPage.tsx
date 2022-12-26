7093c36aff97449497da272981d3fcf1dd214509
cagridemirtash <cagridemirtash@gmail.com>
2022-12-26 20:03:20 +0300

    Handling Data
    Add type reference for error page.

--- front/src/components/Error/ErrorPage.tsx
+++ front/src/components/Error/ErrorPage.tsx
@@ -1,13 +1,14 @@
 import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
+  const error:any = useRouteError();
+  console.error(error.data);
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
+      {error.data}
     </div>
   );
 }
