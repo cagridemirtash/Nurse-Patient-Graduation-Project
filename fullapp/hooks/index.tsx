1e95d8b406890fbde51cbe0daa0b3508a2c56a64
Çağrı Demirtaş <cagridemirtash@gmail.com>
2023-01-02 00:22:03 +0300

    Add structure

---
+++ fullapp/hooks/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Hooks() {
+  return <div>This is a hooks folder</div>;
+}
---
+++ fullapp/utils/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Util() {
+  return <div>This is a utils folder</div>;
+}
