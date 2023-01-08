061d0c33820e24ab919d574ec7f51767155121cc
Çağrı Demirtaş <cagridemirtash@gmail.com>
2023-01-08 19:32:45 +0300

    Patient Detail
    Add Patient card for update complaint about patient from patient

--- fullapp/components/patient/PatientSelectDetail.tsx
+++ fullapp/components/patient/PatientSelectDetail.tsx
@@ -1,10 +1,20 @@
+import Link from "next/link";
+import PatientCard from "./PatientCard";
+
 export default function PatientSelectDetail({ fullName }: any) {
+  const detailArr = [
+    "Ağrım var",
+    "Midem bulanıyor",
+    "Karnım Acıktı",
+    "Başım ağrıyor",
+    "Boşaltım sorunlarım var",
+    "Ateşim var",
+  ];
   return (
     <>
-      <h1>
-        Patient Select dashboard which is detail information about Complaint.
-        {fullName}
-      </h1>
+      {detailArr.map((detail) => (
+        <PatientCard key={detail} fullName={fullName} detail={detail} />
+      ))}
     </>
   );
 }
